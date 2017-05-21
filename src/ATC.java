e2a58f82bd7ce2d625451b7377e083cbeb1c9763
zandoli <vaughnaimable@gmail.com>
2017-05-21 10:37:54 -0400

    Initial

--- src/ATC.java
+++ src/ATC.java
@@ -3,36 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
-
 public class ATC {
+    private static double ovr(double sal, double hrs, double hrswh, double mls){
+        double x = (sal/20/6/4)*hrs*4*2.5;
+        double y = (sal/20/6/4)*hrswh*4*3;
+        return x+y+ mls*32;
+    }
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Enter base salary: ");
         String input1 = br.readLine();
-        double salary = Double.parseDouble(input1);
+        double sal = Double.parseDouble(input1);
 
-        System.out.println("Enter number of 15 minutes worked on weekdays: ");
+        System.out.println("Enter number of hours and part thereof(eg. 0.25, 0.5, 0.75) worked on weekdays: ");
         String input2 = br.readLine();
-        double minutes = Double.parseDouble(input2);
+        double hrs = Double.parseDouble(input2);
 
-	    System.out.println("Enter number of 15 minutes worked on weekends and holidays: ");
+        System.out.println("Enter number of hours and part thereof(eg. 0.25, 0.5, 0.75) worked on weekends and holidays: ");
         String input3 = br.readLine();
-        double mins = Double.parseDouble(input3);
+        double hrswh = Double.parseDouble(input3);
 
         System.out.println("Enter number of meals: ");
         String input4 = br.readLine();
-        double meals = Double.parseDouble(input4);
-
-        double x = (salary/20/6/4)*minutes*2.5;
-	    double y = (salary/20/6/4)*mins*3;
-
-	    double z = x+y+ meals*32;
-	
+        double mls = Double.parseDouble(input4);
 
         DecimalFormat num2Places = new DecimalFormat("##.00");
-        String overtime = "$" + num2Places.format(z);
-
+        String overtime = "$" + num2Places.format(ovr(sal,hrs,hrswh,mls));
         System.out.println("Overtime to be paid: "+overtime);
     }
 }
